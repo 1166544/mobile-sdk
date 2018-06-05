715034c0f156a432127357e47f4dc495caa67f96
jamesliauw <jamesliauw@163.com>
2018-06-05 14:19:19 +0800

    feat: add context extend

--- app/extend/context.ts
+++ app/extend/context.ts
@@ -1,21 +1,7 @@
 import { Context } from 'egg';
 
-/**
- * Context扩展
- */
-class ContextExtender {
-    
-    constructor() {
-        // hole
-    }
-
-    /**
-     * 判断是否为AJAX请求
-     * @param this 
-     */
-    public isAjax(this: Context) {
-        return this.get('X-Requested-With') === 'XMLHttpRequest';
-    }
-
-}
-export default new ContextExtender();
+export default {
+    isAjax(this: Context) {
+      return this.get('X-Requested-With') === 'XMLHttpRequest';
+    },
+};
