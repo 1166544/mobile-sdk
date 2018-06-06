f112c229359831862fedade28990c4c35d3e36e7
jamesliauw <jamesliauw@163.com>
2018-06-06 13:45:21 +0800

    feat: simple run code

--- build/webpack.sync.js
+++ build/webpack.sync.js
@@ -4,61 +4,67 @@ const childProcess = require("child_process");
 const workerProcess = childProcess.exec("npm run webpack", {});
 const socket = require("socket.io-client")("http://127.0.0.1:7001");
 
-// 预处理
-processWebpack();
-processBrowserSync();
-processSocket();
+class SyncProcessor {
 
-/**
- * 处理打包
- */
-function processWebpack() {
-    workerProcess.stdout.on("data", function(data) {
-        console.log("stdout: " + data);
-    });
-    workerProcess.stderr.on("data", function(data) {
-        console.log("stderr: " + data);
-    });
-}
+    constructor() {
+        this.processWebpack();
+        this.processBrowserSync();
+        this.processSocket();
+    }
 
-/**
- * Run Browsersync with server config
- */
-function processBrowserSync() {
-    browserSync({
-        files: [],
-        host: "localhost",
-        open: false,
-        prot: 8088,
-        proxy: {
-            reqHeaders: function(config) {
-                return {
-                    "accept-encoding": "identity",
-                    "agent":           false,
-                    "host":            "localhost",
-                };
+    /**
+     * 处理打包
+     */
+    processWebpack() {
+        workerProcess.stdout.on("data", function(data) {
+            console.log("stdout: " + data);
+        });
+        workerProcess.stderr.on("data", function(data) {
+            console.log("stderr: " + data);
+        });
+    }
+
+    /**
+     * Browsersync
+     */
+    processBrowserSync() {
+        browserSync({
+            files: [],
+            host: "localhost",
+            open: false,
+            prot: 8088,
+            proxy: {
+                reqHeaders: function(config) {
+                    return {
+                        "accept-encoding": "identity",
+                        "agent":           false,
+                        "host":            "localhost",
+                    };
+                },
+                target: "localhost:7001",
             },
-            target: "localhost:7001",
-        },
-    });
+        });
+    }
+
+    /**
+     * 处理连接SOCKET
+     */
+    processSocket() {
+        socket.on("connect", (e) => {
+            console.log("连接成功 ", e);
+
+            const chatStr = "request::reload";
+            console.log("发送消息", chatStr);
+            socket.emit("chat", chatStr);
+        });
+
+        socket.on("res", msg => {
+            if (msg && msg.reload) {
+                console.log(msg.message);
+                browserSync.reload();
+            }
+        });
+    }
 }
 
-/**
- * 处理连接SOCKET
- */
-function processSocket() {
-    socket.on("connect", (e) => {
-        console.log("连接成功 ", e);
-
-        const chatStr = "request::reload";
-        console.log("发送消息", chatStr);
-        socket.emit("chat", chatStr);
-    });
-
-    socket.on("res", msg => {
-        if (msg && msg.reload) {
-            console.log(msg.message);
-            browserSync.reload();
-        }
-    });
-}
+module.exports = new SyncProcessor();
