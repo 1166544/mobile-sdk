1c9d31fde9604eff640350542f1c4b62c3a00439
jamesliauw <jamesliauw@163.com>
2018-06-07 11:19:45 +0800

    feat: add assets

--- app/controller/test/index/index.ts
+++ app/controller/test/index/index.ts
@@ -4,11 +4,11 @@ export default class TestIndexController extends Controller {
   public async list() {
     const pageSize: number = this.app.config.news.pageSize;
     const page: number = parseInt(this.ctx.query.page, 10) || 1;
-    const idList: any[] = await this.ctx.service.newsService.getTopStories(page);
+    // const idList: any[] = await this.ctx.service.newsService.getTopStories(page);
 
     // get itemInfo parallel
-    const newsList: any[] = await Promise.all(idList.map((id) => this.ctx.service.newsService.getItem(id)));
-    await this.ctx.render('test/index/index.html', { list: newsList, page, pageSize });
+    // const newsList: any[] = await Promise.all(idList.map((id) => this.ctx.service.newsService.getItem(id)));
+    await this.ctx.render('test/index/index.html', { list: ['newsList'], page, pageSize });
   }
 
 }
--- client/view/test/index/index.ts
+++ client/view/test/index/index.ts
@@ -1,23 +1,18 @@
 // 导入公共样式
-import "path_styles/libs/bootstrap.css";
-import "path_styles/libs/jquery.countdown.css";
-import "path_styles/libs/animate.min.css";
-import "path_styles/common/css/style.css";
+import '../../../styles/common/css/style.css';
 
 // 导入当前页面样式
-import "./index.scss";
+import './index.scss';
 
+// 当前页面逻辑
+export default class TestIndex {
+    constructor() {
+        window.addEventListener('load', () => {
+            setTimeout(this.hideURLbar, 0); 
+        }, false);
+    }
 
-// JQ调用示例
-// document.querySelector("div").innerHTML = "after bundle1";
-// $("div").eq(1).css({ width: "400px", heihgt: "400px"});
-
-// addEventListener("load", 
-// function() { 
-//     setTimeout(hideURLbar, 0); 
-// }, false);
-// function hideURLbar(){ 
-//     window.scrollTo(0,1); 
-// }
-
-// new WOW().init();
+    public hideURLbar(): void { 
+        window.scrollTo(0, 1);    
+    }
+}
