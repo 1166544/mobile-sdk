604f53f4ecb0e1d641577c42e40bd6bf65d128a9
jamesliauw <jamesliauw@163.com>
2018-06-09 13:13:49 +0800

    feat: add scss

--- client/view/test/index/index.ts
+++ client/view/test/index/index.ts
@@ -1,8 +1,8 @@
 // 导入公共样式
-// import '../../../styles/common/css/style.css';
+import '../../../styles/common/css/style.css';
 
 // 导入当前页面样式
-import './index.css';
+import './index.scss';
 
 // 当前页面逻辑
 export default class TestIndex {
