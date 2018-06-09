191088a73142a2612598c18602cf741fb44da099
jamesliauw <jamesliauw@163.com>
2018-06-09 17:59:25 +0800

    feat: add global sample

---
+++ client/view/news/index/index.ts
@@ -0,0 +1,9 @@
+
+// 导入其它模块逻辑
+import GlobalLogical from '../../../components/global';
+
+// 实例化全局对像
+const global: GlobalLogical = new GlobalLogical();
+
+console.log(global.a);
+console.log(global.b);
