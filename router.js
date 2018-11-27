278fc9bca5e8f58415b592021dbb215658158b9f
James Liauw <jamesliauw@163.com>
2018-11-27 14:06:31 +0800

    feat: 定义常量

--- router.js
+++ router.js
@@ -1,9 +1,10 @@
 'use strict';
+const SocketEngine = require('./engine/SocketEngine');
 
 module.exports = app => {
   // app.io.of('/')
-  app.io.route('requestMessage', app.io.controller.chat.index);
+  app.io.route(SocketEngine.REQUEST_MESSAGE, app.io.controller.chat.index);
 
-  // app.io.of('/chat')
-  app.io.of('/requestMessage').route('requestMessage', app.io.controller.chat.index);
+  // app.io.of('/requestMessage')
+  app.io.of(`/${SocketEngine.REQUEST_MESSAGE}`).route(SocketEngine.REQUEST_MESSAGE, app.io.controller.chat.index);
 };
